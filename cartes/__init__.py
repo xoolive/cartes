093318f2cfb9793ecd644f4c298853def7e9b349
Xavier Olive <git@xoolive.org>
2021-01-30 23:52:34 +0100

    pep 517

--- cartes/__init__.py
+++ cartes/__init__.py
@@ -1,4 +1,6 @@
+import importlib_metadata
+
 # Monkey-patches GeoAxesSubplot for .set_extent()
 from .utils import geoaxes  # noqa: F401
 
-__version__ = "1.0"
+__version__ = importlib_metadata.version("cartes")
