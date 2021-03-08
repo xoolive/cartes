32931740a33c8b5d087476f45ab43cd3127cee57
Xavier Olive <git@xoolive.org>
2021-03-08 23:39:41 +0100

    building queries

---
+++ cartes/osm/overpass/query.py
@@ -0,0 +1,200 @@
+import logging
+import re
+from abc import ABC, abstractmethod
+from numbers import Real
+from typing import Any, Dict, List, Mapping, Optional, Union
+
+QueryType = Union[bool, int, str, List, Mapping[str, Any]]
+
+
+class Generator(ABC):
+    def __set_name__(self, owner, name):
+        self.public_name = name
+        self.private_name = "_" + name
+
+    def __get__(self, obj, objtype=None) -> str:
+        elt = getattr(obj, self.private_name, None)
+        if elt is None:
+            return ""
+        return self.generate(elt, obj)
+
+    def __set__(self, obj, value):
+        if value is not None:
+            value = self.transform(value)
+            self.validate(value)
+        setattr(obj, self.private_name, value)
+
+    def transform(self, value) -> Any:
+        return value
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+    @abstractmethod
+    def generate(self, elt, obj) -> str:
+        pass
+
+
+def expand(key: str, value: Union[bool, str, Dict[str, str]]):
+    if not re.match(r"^[\w\d_]+$", key):
+        key = f'"{key}"'
+    if isinstance(value, bool):
+        return f"[{key}]"
+    if isinstance(value, Real):
+        return f"[{key}={value}]"
+    if isinstance(value, str):
+        if not re.match(r"^[\w\d_]+$", value):
+            value = f'"{value}"'
+        return f"[{key}={value}]"
+    if "regex" in value:
+        value = value["regex"]
+        return f'[{key}~"{value}"]'
+    raise ValueError(f"Unknown elements in area dictionary {value}")
+
+
+class Area(Generator):
+    def validate(self, value):
+        if not isinstance(value, dict):
+            raise TypeError("The area parameter must be a dictionary")
+        for elt in value.values():
+            if not any(isinstance(elt, t) for t in [Real, str, dict]):
+                raise TypeError(
+                    "All values in the area dictionary must be either "
+                    "numeric, str or a dict[str, str]"
+                )
+
+    def generate(self, elt, obj) -> str:
+        res = ";"
+        if "as_" in elt:
+            res = f"->.{elt['as_']};"
+            del elt["as_"]
+        return (
+            "area"
+            + "".join(expand(key, value) for key, value in elt.items())
+            + res
+        )
+
+
+class Bounds(Generator):
+    def transform(self, value):
+        if hasattr(value, "shape"):
+            value = value.shape.bounds
+        return value
+
+    def validate(self, value):
+        try:
+            west, south, east, north = value
+            assert all(isinstance(x, Real) for x in value)
+        except Exception as exc:
+            logging.warning(exc)
+            raise TypeError(
+                "The bounds attribute must be a shape object or "
+                "a tuple of four float (west, south, east, north)"
+            )
+
+    def generate(self, value, obj) -> str:
+        west, south, east, north = value
+        return f"[bbox:{south},{west},{north},{east}]"
+
+
+class NodeWayRel(Generator):
+    def transform(self, value):
+        if isinstance(value, dict):
+            if not any(x in value for x in ["node", "way", "rel", "nwr"]):
+                value = dict(nwr=value)
+            value = [value]
+        return value
+
+    def validate(self, value):
+        pass
+
+    def generate(self, value, obj) -> str:
+        return "".join(self.generate_single(elt, obj) for elt in value)
+
+    def generate_single(self, elt, obj) -> str:
+        for res, elt in elt.items():
+            break
+        # Particular situation of pivot relations
+        if res == "rel" and list(elt.keys()) == ["area"]:
+            res += f"(pivot.{elt['area']});out geom;"
+            return res
+        if getattr(obj, "_area", None) is not None:
+            area_name = elt.get("area", None)
+            if area_name:
+                del elt["area"]
+                res += f"(area.{area_name})"
+            else:
+                res += "(area)"
+        if "around" in elt:
+            res += f"(around:{elt['around']})"
+            del elt["around"]
+        res += "".join(expand(key, value) for key, value in elt.items())
+        res += ";out geom;"
+        return res
+
+
+def make_querytype(
+    values: Optional[QueryType] = None, key="nwr"
+) -> List[QueryType]:
+    if values is None:
+        return []
+    if isinstance(values, list):
+        return list(
+            (
+                {key: elt}
+                if not any(
+                    x in elt.keys() for x in ["node", "way", "rel", "nwr"]
+                )
+                else elt
+            )
+            for elt in values
+        )
+    else:
+        return [{key: values}]
+
+
+class Query:
+    area = Area()
+    bounds = Bounds()
+    nwr = NodeWayRel()
+
+    def __init__(
+        self,
+        *,
+        out: str = "json",
+        timeout: int = 180,
+        **kwargs: QueryType,
+    ) -> None:
+        self.out = out
+        self.timeout = timeout
+        self.area = kwargs.get("area", None)
+        self.bounds = kwargs.get("bounds", None)
+
+        node = make_querytype(kwargs.get("node", None), "node")
+        way = make_querytype(kwargs.get("way", None), "way")
+        rel = make_querytype(kwargs.get("rel", None), "rel")
+        nwr = make_querytype(kwargs.get("nwr", None), "nwr")
+
+        kwargs = dict(
+            (key, value)
+            for key, value in kwargs.items()
+            if key not in ["area", "bounds", "node", "way", "rel", "nwr"]
+        )
+        if len(kwargs):
+            nwr.append(dict(nwr=kwargs))
+
+        self.nwr = nwr + node + way + rel
+
+    def generate(self) -> str:
+        res = f"[out:{self.out}][timeout:{self.timeout}]{self.bounds};"
+        res += f"{self.area}"
+        res += f"{self.nwr}"
+        return res
+
+
+def generate_query(
+    *, out: str = "json", timeout: int = 180, **kwargs: QueryType
+) -> str:
+    query = Query(out=out, timeout=timeout, **kwargs)
+    return query.generate()
--- cartes/tests/test_overpass.py
+++ cartes/tests/test_overpass.py
@@ -1,5 +1,6 @@
 import pytest
-from cartes.osm.overpass import Overpass
+
+from cartes.osm.overpass import Overpass, generate_query
 
 
 def test_basic_query() -> None:
@@ -10,13 +11,101 @@ def test_basic_query() -> None:
 
 
 def test_validator() -> None:
-    query_mercantour = """[out:json];
-area["boundary"="protected_area"]["name"~"Mercantour"]->.a;
-rel(pivot.a);
-out geom;
-node[natural=peak](area.a);
-out geom;
-"""
+    query_mercantour = (
+        "[out:json][timeout:180];"
+        "area['boundary'='protected_area']['name'~'Mercantour']->.a;"
+        "rel(pivot.a);"
+        "out geom;"
+        "node[natural=peak](area.a);"
+        "out geom;"
+    )
     mercantour = Overpass.request(query=query_mercantour)
     with pytest.raises(TypeError):
         mercantour.drop(columns=["geometry"])
+
+
+def test_generate_query() -> None:
+    assert (
+        generate_query(area=dict(icao="LFBO"), aeroway=True)
+        == "[out:json][timeout:180];"
+        "area[icao=LFBO];nwr(area)[aeroway];out geom;"
+    )
+
+    south, west, north, east = 33, -30, 70, 35
+    assert (
+        generate_query(
+            bounds=[west, south, east, north], node=dict(capital="yes")
+        )
+        == "[out:json][timeout:180][bbox:33,-30,70,35];"
+        "node[capital=yes];out geom;"
+    )
+
+    mercantour = (
+        "[out:json][timeout:180];"
+        'area[boundary=protected_area][name~"Mercantour"]->.a;'
+        "rel(pivot.a);"
+        "out geom;"
+        "node(area.a)[natural=peak];"
+        "out geom;"
+    )
+    assert (
+        generate_query(
+            area=dict(
+                boundary="protected_area",
+                name=dict(regex="Mercantour"),
+                as_="a",
+            ),
+            nwr=[
+                dict(rel=dict(area="a")),
+                dict(node=dict(natural="peak", area="a")),
+            ],
+        )
+        == mercantour
+    )
+
+    toulouse = (
+        "[out:json][timeout:180];"
+        "area[name=Toulouse][admin_level=8];"
+        "rel(area)[boundary=postal_code];out geom;"
+    )
+
+    assert (
+        generate_query(
+            area=dict(name="Toulouse", admin_level=8),
+            rel=dict(boundary="postal_code"),
+        )
+        == toulouse
+    )
+
+    cannes = (
+        "[out:json][timeout:180];"
+        "area[name=Cannes][admin_level=8];"
+        "nwr(area)[building];"
+        "out geom;"
+    )
+
+    assert (
+        generate_query(area=dict(name="Cannes", admin_level=8), building=True)
+        == cannes
+    )
+
+    neste = (
+        "[out:json][timeout:180];"
+        'rel[waterway=canal][name~"Neste"];'
+        "out geom;"
+        "rel(around:1000)[waterway=river];"
+        "out geom;"
+        'rel[waterway=river][name="La Garonne"];'
+        "out geom;"
+    )
+
+    assert (
+        generate_query(
+            rel=[
+                dict(waterway="canal", name=dict(regex="Neste")),
+                dict(around=1000, waterway="river"),
+                dict(waterway="river", name="La Garonne"),
+            ]
+        )
+        == neste
+    )
