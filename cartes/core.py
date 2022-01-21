590c125b809367e2247a8b74fbb46cbf8aaf32ba
Xavier Olive <git@xoolive.org>
2022-01-21 19:25:15 +0100

    leaflet output

--- cartes/core.py
+++ cartes/core.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from abc import ABC, abstractproperty
-from typing import Tuple
+from typing import TYPE_CHECKING, Any, Iterator, Tuple
 
 from cartopy.crs import Orthographic
 
@@ -7,6 +9,9 @@ import pandas as pd
 from pyproj import Proj, Transformer
 from shapely.geometry import base, shape
 from shapely.ops import transform
+
+if TYPE_CHECKING:
+    from ipyleaflet import Map, Marker, Polygon, Polyline
 
 
 class GeoObject(ABC):
@@ -82,3 +87,84 @@ class GeoObject(ABC):
         from .crs.info import valid_crs
 
         return valid_crs(self)
+
+    def zoom_level(self) -> int:
+        x1, y1, x2, y2 = self.bounds
+
+        if x1 == x2:
+            return 17
+
+        def func(n):
+            factor = (360 * 2 ** -n) / (x2 - x1)
+            if factor < 1:
+                return 1 / factor
+            return factor
+
+        return min(range(20), key=func)
+
+    def leaflet(
+        self, **kwargs: Any
+    ) -> None | "Polygon" | "Polyline" | "Marker":
+        """Returns a Leaflet layer to be directly added to a Map.
+
+        .. warning::
+            This is only available if the Leaflet `plugin <plugins.html>`_ is
+            activated. (true by default)
+
+        The elements passed as kwargs as passed as is to the Polygon __init__
+        method.
+        """
+        from ipyleaflet import Marker, Polygon, Polyline
+
+        kwargs = {**dict(weight=3), **kwargs}
+        coords: list[Any] = []
+
+        def unfold(shape: Any) -> Iterator[Any]:
+            yield shape.exterior
+            yield from shape.interiors
+
+        if self.shape.geom_type == "Polygon":
+            coords = list(
+                list((lat, lon) for (lon, lat, *_) in x.coords)
+                for x in unfold(self.shape)
+            )
+            return Polygon(locations=coords, **kwargs)
+        if self.shape.geom_type == "MultiPolygon":
+            coords = list(
+                list((lat, lon) for (lon, lat, *_) in x.coords)
+                for piece in self.shape.geoms
+                for x in unfold(piece)
+            )
+            return Polygon(locations=coords, **kwargs)
+        if self.shape.geom_type == "LineString":
+            return Polyline(
+                locations=list((lat, lon) for (lon, lat) in self.shape.coords),
+                **kwargs,
+            )
+        if self.shape.geom_type == "MultiLineString":
+            return Polyline(
+                locations=list(
+                    list((lat, lon) for (lon, lat) in piece.coords)
+                    for piece in self.shape.geoms
+                ),
+                **kwargs,
+            )
+        if self.shape.geom_type == "Point":
+            (lon, lat), *_ = self.shape.coords
+            return Marker(location=(lat, lon), **kwargs)
+
+        return None
+
+    def map_leaflet(self, **kwargs) -> "Map":
+        from ipyleaflet import Map
+
+        x1, y1, x2, y2 = self.bounds
+        m = Map(
+            center=(0.5 * (y1 + y2), 0.5 * (x1 + x2)), zoom=self.zoom_level()
+        )
+
+        layer = self.leaflet(**kwargs)
+        if layer is not None:
+            m.add_layer(layer)
+
+        return m
